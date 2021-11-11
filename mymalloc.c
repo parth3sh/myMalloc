bb46a7a38268f054abe2393d7336c88781e63529
Abdul K. Uddin <aku10@ilab4.cs.rutgers.edu>
2021-11-11 16:56:29 -0500

    got first and nextfit working

--- mymalloc.c
+++ mymalloc.c
@@ -22,19 +22,23 @@ static memBlock* freeHead;
 static memBlock* lastUsed;
 
 int main(int argc, char* argv[]) {
-	myinit(0);
+	myinit(2);
 	int* ptr1 = (int*)mymalloc(12);
 	void* ptr2 = mymalloc(5);
 	char* ptr3 = (char*)mymalloc(35);
-	void* ptr4 = mymalloc(7);
-	//uintptr_t p1 = (uintptr_t)(ptr1);
-	//uintptr_t p2 = (uintptr_t)(ptr2);
-	//uintptr_t p3 = (uintptr_t)(ptr3);
-	//uintptr_t p4 = (uintptr_t)(ptr4);
+	void* ptr4 = mymalloc(1);
+	uintptr_t p1 = (uintptr_t)(ptr1);
+	uintptr_t p2 = (uintptr_t)(ptr2);
+	uintptr_t p3 = (uintptr_t)(ptr3);
+	uintptr_t p4 = (uintptr_t)(ptr4);
 	printf("p1 = %p\n", ptr1);
 	printf("p2 = %p\n", ptr2);
 	printf("p3 = %p\n", ptr3);
 	printf("p4 = %p\n", ptr4);
+	printf("p1 = %ld\n", p1%8);
+	printf("p2 = %ld\n", p2%8);
+	printf("p3 = %ld\n", p3%8);
+	printf("p4 = %ld\n", p4%8);
 }
 
 
@@ -60,51 +64,111 @@ void split(memBlock* ptr, size_t size){
 
 void* firstFit(size_t size){
 	memBlock* ptr = freeHead;
+	memBlock* prev = NULL;
 	while(ptr!=NULL){
 
 		if(ptr->size == size){
 			ptr->free = 0;
 			lastUsed = ptr->nextFree;
+			if (prev == NULL) {
+				freeHead = ptr->nextFree;
+				if (freeHead)
+					freeHead->prevFree = NULL;
+			} else {
+				prev->nextFree = ptr->nextFree;
+				if (ptr->nextFree)
+					ptr->nextFree->prevFree = prev;
+			}
 			return (void*) (++ptr);			
 		}
 
 		else if(ptr->size > size){
 			split(ptr, size);
+			if (prev == NULL) {
+				freeHead = ptr->nextFree;
+				if (freeHead)
+					freeHead->prevFree = NULL;
+			} else {
+				prev->nextFree = ptr->nextFree;
+				if (ptr->nextFree)
+					ptr->nextFree->prevFree = prev;
+			}
 			return (void*) (++ptr);
 		}
 
+		prev = ptr;
 		ptr = ptr->nextFree;
 	}
 	return NULL;
 }
 
 void* nextFit(size_t size){
-	memBlock* ptra = lastUsed;
-	while(ptra!=NULL){
-		if(ptra->size == size){
-			ptra->free = 0;
-			lastUsed = ptra->nextFree;
-			return (void*) ptra;			
+	memBlock* ptr = lastUsed;
+	memBlock* prev = NULL;
+	while(ptr!=NULL){
+		if(ptr->size == size){
+			ptr->free = 0;
+			lastUsed = ptr->nextFree;
+			if (prev == NULL) {
+				freeHead = ptr->nextFree;
+				if (freeHead)
+					freeHead->prevFree = NULL;
+			} else {
+				prev->nextFree = ptr->nextFree;
+				if (ptr->nextFree)
+					ptr->nextFree->prevFree = prev;
+			}
+			return (void*) ptr;			
 		}
-		else if(ptra->size > size){
-			split(ptra, size);
-			return (void*) ptra;
+		else if(ptr->size > size){
+			split(ptr, size);
+			if (prev == NULL) {
+				freeHead = ptr->nextFree;
+				if (freeHead)
+					freeHead->prevFree = NULL;
+			} else {
+				prev->nextFree = ptr->nextFree;
+				if (ptr->nextFree)
+					ptr->nextFree->prevFree = prev;
+			}
+			return (void*) ptr;
 		}
-		ptra = ptra->nextFree;
+		prev = ptr;
+		ptr = ptr->nextFree;
 	}
-	ptra = freeHead;
+	ptr = freeHead;
+	prev = NULL;
 
-	while(ptra!=NULL && ptra!=lastUsed){
-		if(ptra->size == size){
-			ptra->free = 0;
-			lastUsed = ptra->nextFree;
-			return (void*) ptra;			
+	while(ptr!=NULL && ptr!=lastUsed){
+		if(ptr->size == size){
+			ptr->free = 0;
+			lastUsed = ptr->nextFree;
+			if (prev == NULL) {
+				freeHead = ptr->nextFree;
+				if (freeHead)
+					freeHead->prevFree = NULL;
+			} else {
+				prev->nextFree = ptr->nextFree;
+				if (ptr->nextFree)
+					ptr->nextFree->prevFree = prev;
+			}
+			return (void*) ptr;			
 		}
-		else if(ptra->size > size){
-			split(ptra, size);
-			return (void*) ptra;
+		else if(ptr->size > size){
+			split(ptr, size);
+			if (prev == NULL) {
+				freeHead = ptr->nextFree;
+				if (freeHead)
+					freeHead->prevFree = NULL;
+			} else {
+				prev->nextFree = ptr->nextFree;
+				if (ptr->nextFree)
+					ptr->nextFree->prevFree = prev;
+			}
+			return (void*) ptr;
 		}
-		ptra = ptra->nextFree;
+		prev = ptr;
+		ptr = ptr->nextFree;
 	}
 	return NULL;
 }
@@ -112,6 +176,7 @@ void* nextFit(size_t size){
 
 void *bestFit(size_t size) {
 	memBlock* ptr = freeHead;
+	memBlock* prev = NULL;
 	memBlock* best = NULL;
 	int dis = INT_MAX;
 	while(ptr!=NULL){
@@ -119,15 +184,31 @@ void *bestFit(size_t size) {
 		if(ptr->size == size){
 			ptr->free = 0;
 			lastUsed = ptr->nextFree;
+			if (prev == NULL) {
+				freeHead = ptr->nextFree;
+				if (freeHead)
+					freeHead->prevFree = NULL;
+			} else {
+				prev->nextFree = ptr->nextFree;
+				if (ptr->nextFree)
+					ptr->nextFree->prevFree = prev;
+			}
 			return (void*) ptr;			
 		}
 		else if(ptr->size > size && (ptr->size-size < dis)){
 			dis = ptr->size - size;
 			best = ptr;
 		}
+		prev = ptr;
 		ptr = ptr->nextFree;
 	}
-	if (ptr) {
+	if (best) {
+		if (best->prevFree)
+			best->prevFree->nextFree = best->nextFree;
+		else
+			freeHead = best->nextFree;
+		if (best->nextFree)
+			best->nextFree->prevFree = best->prevFree;
 		ptr = best;
 		split(ptr, size);
 		return ptr;
