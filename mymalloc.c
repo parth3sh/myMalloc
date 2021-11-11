0f03093ef3a5f623c91ff5e217e0c1b881ca7ceb
Abdul K. Uddin <aku10@ilab4.cs.rutgers.edu>
2021-11-11 15:59:31 -0500

    mymalloc changes

--- mymalloc.c
+++ mymalloc.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <limits.h>
 #include <stdlib.h>
 
 static int fitAlgo;
@@ -7,6 +9,7 @@ static char* memory;
 #define MEMSIZE 8
 
 void myinit(int allocAlgo);
+void* mymalloc(size_t size);
 
 typedef struct memBlock {
 	int free;
@@ -16,16 +19,22 @@ typedef struct memBlock {
 } memBlock;
 
 static memBlock* freeHead;
-static memBlock* lastBlock;
+static memBlock* lastUsed;
 
 int main(int argc, char* argv[]) {
-	printf("%d", sizeof(memBlock));
-	myinit(2);
-	for(int n = 0; n < 1024*1024; n++){
-		memory[n] = 'p';
-	}
-	printf("Test: %c %d\n", memory[5], fitAlgo);
-
+	myinit(0);
+	int* ptr1 = (int*)mymalloc(12);
+	void* ptr2 = mymalloc(5);
+	char* ptr3 = (char*)mymalloc(35);
+	void* ptr4 = mymalloc(7);
+	//uintptr_t p1 = (uintptr_t)(ptr1);
+	//uintptr_t p2 = (uintptr_t)(ptr2);
+	//uintptr_t p3 = (uintptr_t)(ptr3);
+	//uintptr_t p4 = (uintptr_t)(ptr4);
+	printf("p1 = %p\n", ptr1);
+	printf("p2 = %p\n", ptr2);
+	printf("p3 = %p\n", ptr3);
+	printf("p4 = %p\n", ptr4);
 }
 
 
@@ -33,78 +42,48 @@ int main(int argc, char* argv[]) {
 void myinit(int allocAlgo){
 	memory = malloc(HEAPSIZE);
 	fitAlgo = allocAlgo;
-	blockHead->free = 1;
-	blockHead->size = HEAPSIZE;
-	blockHead->nextBlock = NULL;
-	blockHead->prevBlock = NULL;
-
-	blockHead->nextFree = NULL;
-	blockHead->prevFree = NULL;
-	freeHead = blockHead;
-}
-
-void* mymalloc(size_t size){
-	tempSize = MEMSIZE;
-	if (size > MEMSIZE){
-		tempSize = MEMSIZE * ((size + (MEMSIZE-1)) / MEMSIZE);
-	}
-	size = tempSize + sizeof(memBlock);
-	if(fitAlgo == 0){
-		firstFit(size);	
-	}
-
-	//secondfit 
-	else if(fitAlgo == 1){
-		nextFit(size);
-	}
-	
-	else if(fitAlgo == 2){
-		bestFit(size);
-}
-
-}
-
-void* firstFit(size_t size){
-	memBlock* ptr = freeHead;
-	while(ptr!=NULL){
-		if(ptr->size < size){
-			continue;
-		}
-		if(ptr->size == size){
-			ptr->free = 0;
-			return (void*) ptr;			
-		}
-		else if(ptr->size > size){
-			split(ptr, size);
-			return (void*) ptr;
-		}
-		ptr = ptr->nextFree;
-	}
-	return NULL;
+	freeHead = (void*)memory;
+	freeHead->size = HEAPSIZE;
 }
 
 void split(memBlock* ptr, size_t size){
 	memBlock *extra=(void*)((void*)ptr+size);
 	extra->size=(ptr->size)-size;
 	extra->free=1;
-	extra->nextBlock=ptr->nextBlock;
-	extra->prevBlock = ptr;
 	extra->nextFree = ptr->nextFree;
 	extra->prevFree = ptr->prevFree;
 	ptr->size=size;
 	ptr->free=0;
-	ptr->nextBlock=extra;
 	ptr->nextFree = extra;
+	lastUsed = extra;
  }
+
+void* firstFit(size_t size){
+	memBlock* ptr = freeHead;
+	while(ptr!=NULL){
+
+		if(ptr->size == size){
+			ptr->free = 0;
+			lastUsed = ptr->nextFree;
+			return (void*) (++ptr);			
+		}
+
+		else if(ptr->size > size){
+			split(ptr, size);
+			return (void*) (++ptr);
+		}
+
+		ptr = ptr->nextFree;
+	}
+	return NULL;
+}
 
 void* nextFit(size_t size){
 	memBlock* ptra = lastUsed;
 	while(ptra!=NULL){
-		if(ptra->size < size){
-			continue;
-		}
 		if(ptra->size == size){
 			ptra->free = 0;
+			lastUsed = ptra->nextFree;
 			return (void*) ptra;			
 		}
 		else if(ptra->size > size){
@@ -116,11 +95,9 @@ void* nextFit(size_t size){
 	ptra = freeHead;
 
 	while(ptra!=NULL && ptra!=lastUsed){
-		if(ptra->size < size){
-			continue;
-		}
 		if(ptra->size == size){
 			ptra->free = 0;
+			lastUsed = ptra->nextFree;
 			return (void*) ptra;			
 		}
 		else if(ptra->size > size){
@@ -129,4 +106,55 @@ void* nextFit(size_t size){
 		}
 		ptra = ptra->nextFree;
 	}
+	return NULL;
 }
+
+
+void *bestFit(size_t size) {
+	memBlock* ptr = freeHead;
+	memBlock* best = NULL;
+	int dis = INT_MAX;
+	while(ptr!=NULL){
+
+		if(ptr->size == size){
+			ptr->free = 0;
+			lastUsed = ptr->nextFree;
+			return (void*) ptr;			
+		}
+		else if(ptr->size > size && (ptr->size-size < dis)){
+			dis = ptr->size - size;
+			best = ptr;
+		}
+		ptr = ptr->nextFree;
+	}
+	if (ptr) {
+		ptr = best;
+		split(ptr, size);
+		return ptr;
+	}
+	return NULL;
+}
+
+void* mymalloc(size_t size){
+	size_t tempSize = MEMSIZE;
+	if (size > MEMSIZE){
+		tempSize = MEMSIZE * ((size + (MEMSIZE-1)) / MEMSIZE);
+	}
+	size = tempSize + sizeof(memBlock);
+	void* ptr = NULL;
+	if(fitAlgo == 0){
+		ptr = firstFit(size);	
+	}
+
+	//secondfit 
+	else if(fitAlgo == 1){
+		ptr = nextFit(size);
+	}
+	
+	else if(fitAlgo == 2){
+		ptr = bestFit(size);
+	}
+	return ptr;
+
+}
+
